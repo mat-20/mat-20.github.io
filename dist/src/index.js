929bd2e7e7ded529af1fa19d7ab59b8a3d595215
mat-20 <108427355+mat-20@users.noreply.github.com>
2025-08-04 15:15:01 -0400

    Update index.js

--- dist/src/index.js
+++ dist/src/index.js
@@ -30,18 +30,15 @@ async function loadSlideshowImages() {
     });
 }
 
-window.addEventListener('DOMContentLoaded', ()=>{
+window.addEventListener('DOMContentLoaded', () => {
     console.log('DOM fully loaded');
-    loadSlideshowImages();
     const savedTheme = localStorage.getItem('theme');
     if (savedTheme === 'pink') {
         document.documentElement.classList.add('theme-pink');
     } else if (savedTheme === 'frog') {
         document.documentElement.classList.add('theme-frog');
     }
-    // Update meta tags dynamically
     updateMetaTags();
-    // Show live time in #time element
     function updateTime() {
         const timeElement = document.getElementById('time');
         if (!timeElement) {
@@ -54,6 +51,3 @@ window.addEventListener('DOMContentLoaded', ()=>{
     updateTime();
     setInterval(updateTime, 1000);
 });
-
-//# sourceMappingURL=index.js.map
-
